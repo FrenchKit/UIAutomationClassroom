96d07568eaf471515a9c81b07000944544796216
Dmitry Bespalov <dmitry.bespalov@zalando.de>
2017-08-25 14:31:05 +0200

    Added AAA example

---
+++ UITestingWorkshopUITests/ArrangeActAssertExample.swift
@@ -0,0 +1,65 @@
+//
+//  Copyright © 2017 Zalando SE. All rights reserved.
+//
+
+import XCTest
+
+class ArrangeActAssertExample: XCTestCase {
+
+    let app = Application.xcApp
+    let mainScreen = MainScreen()
+
+    override func setUp() {
+        super.setUp()
+        Application.launch()
+    }
+
+    func test_allInOne() {
+        mainScreen.showTableButton.tap()
+
+        let cell = app.tables["table"].cells.allElementsBoundByIndex.first
+
+        cell?.tap()
+
+        waitUntil(app.staticTexts["Item"], is: .exists)
+
+        XCTAssertExist(app.staticTexts["Item #1"])
+
+        app.buttons["Back"].tap()
+        waitUntil(app.staticTexts["Table"], is: .exists)
+
+        app.buttons["Back"].tap()
+        waitUntil(app.staticTexts["First Tab"], is: .exists)
+
+        app.buttons["Second"].tap()
+        waitUntil(app.staticTexts["Second Tab"], is: .exists)
+
+        XCTAssertExist(app.staticTexts["OTHER TAB"])
+    }
+
+    func test_whenTableOpened_itHasItems() {
+        mainScreen.showTableButton.tap()
+
+        let cells = app.tables["table"].cells.allElementsBoundByIndex
+
+        XCTAssertFalse(cells.isEmpty)
+    }
+
+    func test_whenTableItemOpened_detailsHasInformation() {
+        mainScreen.showTableButton.tap()
+        let cells = app.tables["table"].cells.allElementsBoundByIndex
+
+        cells.first?.tap()
+
+        waitUntil(app.staticTexts["Item"], is: .exists)
+        XCTAssertExist(app.staticTexts["Item #1"])
+    }
+
+    func test_whenSecondTabSelected_screenExists() {
+        app.buttons["Second"].tap()
+
+        waitUntil(app.staticTexts["Second Tab"], is: .exists)
+        XCTAssertExist(app.staticTexts["OTHER TAB"])
+    }
+
+}
